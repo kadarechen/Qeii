03133e95a9ed424f95c37cb516c44f95bde9f0e3
AntonioChan <kadarechen@qq.com>
2022-06-26 00:12:37 +0100

    Initial Commit

---
+++ Qeii/QeiiApp.swift
@@ -0,0 +1,20 @@
+//
+//  QeiiApp.swift
+//  Qeii
+//
+//  Created by 陈卓学 on 2022/6/26.
+//
+
+import SwiftUI
+
+@main
+struct QeiiApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
