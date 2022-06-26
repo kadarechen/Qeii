cf5373434856993247ed149f71c5e964389f6803
AntonioChan <kadarechen@qq.com>
2022-06-26 03:00:43 +0100

    Finish homepage and going to add SPM

---
+++ Qeii/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Qeii
+//
+//  Created by 陈卓学 on 2022/6/26.
+//
+
+import Foundation
+
+struct Constants {
+    static let backgroundColor = "Blue1"
+    static let budgetModifer = "square.and.pencil"
+    static let progressBarColor = "Blue2"
+    static let progressBarColorGray = "ProgressBarGray"
+    static let categoryGridBGColor = "CategoryGridBGColor"
+    static let categoryTitleColor = "Blue2"
+}
--- Qeii/QeiiApp.swift
+++ Qeii/QeiiApp.swift
@@ -13,8 +13,9 @@ struct QeiiApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+//            ContentView()
+//                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            HomeView()
         }
     }
 }
